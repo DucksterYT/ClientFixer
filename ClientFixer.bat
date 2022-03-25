ed961e7fcd3cbc6f6f9375e1a5accf9a6fdc461f
Duckster <100494298+DucksterYT@users.noreply.github.com>
2022-03-25 18:24:45 -0400

    Add files via upload

---
+++ ClientFixer.bat
@@ -0,0 +1,6 @@
+@echo off
+COLOR 70
+title Client Fixer by DucksterYT
+start %localappdata%\Roblox
+echo Delete GlobalBasicSettings_13.xml and GlobalSettings_13.xml, once you have done that you can close this window and play Roblox again!
+pause
